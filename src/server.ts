3c6d127c4234d13215681bc2ba224de33d030f98
Roberto Bressanelli <rbressanelli@gmail.com>
2022-04-06 08:59:53 -0300

    refactor

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import app from "./app";
+import { productionEnv, developmentEnv } from '../ormconfig'
 
-createConnection();
-// createConnection()
-//   .then(() => {
-//     const PORT = process.env.PORT ?? 3000;
-//     console.log("Database running");
-//     app.listen(PORT, () => console.log(`App is running at localhost:${PORT}`));
-//   })
-//   .catch((err) => console.log(err));
+// createConnection();
+createConnection()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    console.log("Database running");
+    app.listen(PORT, () => console.log(`App is running at localhost:${PORT}`));
+  })
+  .catch((err) => console.log(err));
 
-app.listen(process.env.PORT || 3000, () => console.log(`${process.env.PORT}`)); 
+// app.listen(process.env.PORT || 3000, () => console.log(`${process.env.PORT}`)); 
